7f99c21300eb6319e59bc18f90aec4fe940d6799
Opilane TTHK <opilane@tthk.ee>
2020-09-03 12:18:42 +0300

    first commitgitgt

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace HowOldAreYou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good morning!");
+
+            Console.WriteLine("What is your name?");
+
+            string Name;
+            Name = Console.ReadLine();
+            
+            Console.WriteLine("Hello, " + Name);
+
+            Console.WriteLine("In what year were you born in?");
+            
+            string UserInput;
+            
+            UserInput = Console.ReadLine();
+            
+            int YearOfBirth;
+            
+            YearOfBirth = Int32.Parse(UserInput);
+            
+            int Age = 2020 - YearOfBirth;
+            
+            Console.WriteLine("You are " + Age + "old.");
+
+
+
+
+        
+                
+
+           
+
+            
+
+
+        }
+
+    }
+}
